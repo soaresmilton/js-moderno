337c9f94afaccf8f761810ae08a2ad0bc8dc15b2
soaresmilton <eng.milton.soares@gmail.com>
2021-11-30 16:54:53 -0300

    Aula 09: Funçoes Helper (includes, startsWith e endsWith)

---
+++ 09-helpers.js
@@ -0,0 +1,12 @@
+const tech = "Node.js";
+const arr = ['Node.js', 'React', 'Typescript']
+
+const includesString = tech.includes('Node');
+const includesArray = arr.includes('Node.js')
+// console.log(includesArray);
+
+const startsWith = tech.startsWith('N');
+// console.log({ startsWith });
+
+const endsWith = tech.endsWith('.js');
+console.log({ endsWith });
