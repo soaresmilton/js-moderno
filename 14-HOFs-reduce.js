09c0322d05626243acc40d7c31c571182bd447dc
soaresmilton <eng.milton.soares@gmail.com>
2021-11-30 18:36:01 -0300

    Aula 14: HOFs - Reduce

---
+++ 14-HOFs-reduce.js
@@ -0,0 +1,11 @@
+const array = [
+  { name: 'iPhone', price: 5000, quantity: 2 },
+  { name: 'MacBook Pro', price: 20000, quantity: 1 },
+  { name: 'Magic Mouse', price: 1000, quantity: 5 }
+];
+
+const reduce = array.reduce((accumulator, product, index) => {
+  return accumulator += product.quantity * product.price;
+}, 0)
+
+console.log({ reduce });
