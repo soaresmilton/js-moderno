794fc8a2758e54736ce6fa9b48e39158a227e608
soaresmilton <eng.milton.soares@gmail.com>
2021-11-30 18:16:01 -0300

    Aula 13: HOFs - .filter

---
+++ 13-HOFs-filter.js
@@ -0,0 +1,11 @@
+const array = [
+  { name: 'iPhone', price: 5000, quantity: 2 },
+  { name: 'MacBook Pro', price: 20000, quantity: 1 },
+  { name: 'Magic Mouse', price: 1000, quantity: 5 }
+];
+
+const filter = array.filter((product) => {
+  return product.quantity > 1;
+});
+
+console.log(filter);
